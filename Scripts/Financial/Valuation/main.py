be029b3f65331f790046fe11e66ec54d271dd10e
x1juacas <jcassinerio@fyo.com>
2024-10-09 17:02:55 -0300

    a

--- Scripts/Financial/Valuation/main.py
+++ Scripts/Financial/Valuation/main.py
@@ -66,13 +66,6 @@ def results_plotter(ticker_data,results):
         name='Historic Stock Price'  # Label for the trace
     ), row=1, col=1)
 
-    fig.update_xaxes(title_text="Fecha", row=1, col=1)  # Título del eje x para el primer subplot
-    fig.update_yaxes(title_text="Precio de Cierre Ajustado", row=1, col=1)  # Título del eje y para el primer subplot
-    fig.update_layout(
-        title_text="Precio Histórico de Acciones",  # Título general de la figura
-        # ... otros ajustes de layout
-    )
-
     ball_color = ["green", "blue", "red"]
 
     for i in range(len(results['TarjetPrice_scenarios'])):
@@ -90,7 +83,7 @@ def results_plotter(ticker_data,results):
             name=scenario_name
         ), row=1, col=1)
 
-    y_min = (ticker_data['price']['Adj Close'],results['TarjetPrice_scenarios']['Tarjet Price'].iloc[2]).min()
+    y_min = min(ticker_data['price']['Adj Close'].min(), results['TarjetPrice_scenarios']['Tarjet Price'].iloc[2].min())
     y_max = ticker_data['price']['Adj Close'].max()
 
     # Add the line using the manually defined y-axis range
@@ -108,17 +101,18 @@ def results_plotter(ticker_data,results):
     )
 
 
+
     # Revenue Data
     fig.add_trace(go.Scatter(
         x=results['Projected Financial Statements']['Date'],
-        y=results['Projected Financial Statements']['Revenue Change'],
+        y=results['Projected Financial Statements']['Revenue Change']*100,
         mode='lines',
         line=dict(color='black', width=2, dash='dash'),  # Line color and width
         name='Historic Stock Price'  # Label for the trace
     ), row=2, col=1)
     fig.add_trace(go.Scatter(
         x=results['Projected Financial Statements']['Date'],
-        y=results['Projected Financial Statements']['Smoothed Revenue Change'],
+        y=results['Projected Financial Statements']['Smoothed Revenue Change']*100,
         mode='lines',
         line=dict(color='dark grey', width=2, dash='dash'),  # Line color and width
         name='Historic Stock Price'  # Label for the trace
@@ -182,13 +176,13 @@ def results_plotter(ticker_data,results):
     if results['R2']>60:
         text_parts_22 = [
             f"<b>Intrinsic Value Obtained (DCF):</b>",
-            f"<b>Current Price:</b> {ticker_data['price']['Adj Close'].iloc[0]}<br>",
-            f"<b>Normal Scenario:</b> {results['TarjetPrice_scenarios']['Tarjet Price'].iloc[1]}<br>",
-            f"<b>Optimistic Scenario:</b> {results['TarjetPrice_scenarios']['Tarjet Price'].iloc[0]}<br>",
-            f"<b>Pesimistic Scenario:</b> {results['TarjetPrice_scenarios']['Tarjet Price'].iloc[2]}<br>",
-            f"<b>Status:</b> {action} / {(tp/p_v -1)*100}<br>",
+            f"<b>Current Price:</b> ${round(ticker_data['price']['Adj Close'].iloc[0],1)}<br>",
+            f"<b>Normal Scenario:</b> ${round(results['TarjetPrice_scenarios']['Tarjet Price'].iloc[1],1)}<br>",
+            f"<b>Optimistic Scenario:</b> ${round(results['TarjetPrice_scenarios']['Tarjet Price'].iloc[0],1)}<br>",
+            f"<b>Pesimistic Scenario:</b> ${round(results['TarjetPrice_scenarios']['Tarjet Price'].iloc[2],1)}<br>",
+            f"<b>Status:</b> {action} / {round((tp/p_v -1)*100,1)} %<br>",
         ]
-        text_sizes_22 = [20, 15, 15, 15, 15, 30]
+        text_sizes_22 = [20, 15, 15, 15, 15, 20]
         text_colors_22 = ["black", "black", "black", "black","black", "black"]
     else: #not trustable results
         text_parts_22 = [f"<b>No Value. Low Accuracy Result:</b>"]
@@ -198,26 +192,59 @@ def results_plotter(ticker_data,results):
     annotations_22 = []
     for i in range(len(text_parts_22)):
         annotations_22.append(dict(text=text_parts_22[i],x=0.77,y=0.3 - i * 0.05,showarrow=False,font=dict(size=text_sizes_22[i], color=text_colors_22[i]),xref="paper", yref="paper",xanchor="left"))
-    all_annotations = annotations_12+annotations_22
+
+    annotations_22 = []
+    for i in range(len(text_parts_22)):
+        annotations_22.append(dict(text=text_parts_22[i], x=0.77, y=0.3 - i * 0.05, showarrow=False,
+                                   font=dict(size=text_sizes_22[i], color=text_colors_22[i]), xref="paper",
+                                   yref="paper", xanchor="left"))
+    annotations_11 = [dict(
+        text="<b>Historic Stock Price [usd]</b>",
+        x=0,  # Centered horizontally
+        y=1.05,  # Slightly above the subplot
+        xref="paper",  # Reference to entire figure width
+        yref="paper",  # Reference to entire figure height
+        showarrow=False,  # No arrow
+        font=dict(size=20, color="black")  # Adjust font size and color
+    )]
+    annotations_21 = [dict(
+        text="<b>Revenue Growth (Quaterly %)</b>",
+        x=0,  # Centered horizontally
+        y=0.38,  # Slightly above the subplot
+        xref="paper",  # Reference to entire figure width
+        yref="paper",  # Reference to entire figure height
+        showarrow=False,  # No arrow
+        font=dict(size=20, color="black",)  # Adjust font size and color
+    )]
+
+    annotations_title = [dict(
+        text=f"Fundamental Stock Value of <b>{ticker}</b> for last Financial Statement : <b>{results['Date_t0'].strftime('%Y-%m-%d')}</b>",
+
+        x=0,  # Centered horizontally
+        y=1.12,  # Slightly above the subplot
+        xref="paper",  # Reference to entire figure width
+        yref="paper",  # Reference to entire figure height
+        showarrow=False,  # No arrow
+        font=dict(size=30, family="Helvetica", color="black")  # Adjust font size and color
+    )]
+
+    all_annotations = annotations_12 + annotations_22 + annotations_11 + annotations_21 + annotations_title
+    # Assuming 'Date' is a string column containing dates in YYYY-MM-DD format
+
     fig.update_layout(
         annotations=all_annotations,
-        title=dict(
-            text=f"Fundamental Stock Value of <b>{ticker}</b> at last presented Financial Statement at {results['Date_t0']}",
-            font=dict(size=30, family="Helvetica", color="black")
-        )
-    )
+        showlegend=False,
+        xaxis1=dict(  # Adjust xaxis1 for the first plot
+            title='Date'
+        ),
+        xaxis2=dict(  # Adjust xaxis2 for the second plot
+            title='Date',  # Set x-axis title
+            #rangeslider=dict(visible=True,  # Show rangeslider for zooming and panningthickness=0.05
+            )  )
 
-    fig.add_annotation(
-        xref="paper", yref="paper",  # Reference the whole plot, but adjust for the subplot
-        x=2, y=2,  # Adjust x and y to position the title over the quadrant
-        showarrow=False,
-        text="Historic Stock Price",  # The title text
-        font=dict(size=50)  # Font size of the title
-    )
 
     fig.show()
     return
-
 
 
 
